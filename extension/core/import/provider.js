8018b0f3ddf5a4104687da54a6c074a5d3f06f39
haohongfei2001-png <haohongfei2001@gmail.com>
2026-09-13 00:00:59 +0800

    fix: retain active import adapter for existing UI contract

--- extension/core/import/provider.js
+++ extension/core/import/provider.js
@@ -3,5 +3,5 @@ import {ImportCoordinator} from './coordinator.js';
 import {getOfficialExportAdapter,officialExportStatus} from './registry.js';
 export class OfficialExportProvider extends HistoryCompletionProvider {
  describe(){return {...officialExportStatus(),contractVersion:2,input:'user_selected_file',network:false};}
- createSession(options){return new ImportCoordinator({...options,resolveAdapter:getOfficialExportAdapter});}
+ createSession(options){return new ImportCoordinator({...options,adapter:getOfficialExportAdapter(),resolveAdapter:getOfficialExportAdapter});}
 }
